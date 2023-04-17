dd21baf3ffc6b0bb06f9d6256401dba7fcbf6393
Kaylie Tipton <kaylietipton23@gmail.com>
2023-04-17 18:18:46 -0500

    STATISTICS SCREEM

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:android_projects/game_cover_screen.dart';
 import 'package:android_projects/games_screen.dart';
 import 'package:flutter/material.dart';
@@ -6,9 +7,10 @@ import 'title_screen.dart';
 void main() {
   runApp(
       MaterialApp(
-        initialRoute: 'main_menu',
+        initialRoute: '/main_menu',
         routes: {
-          'main_menu': (context) => const TitleScreen(),
+          '/main_menu': (context) => const TitleScreen(),
+          '/main_menu/statistics': (context) => const StatisticsScreen(),
           // make sure to change this const ^ when you want to check how your new class
           // looks in the live preview in the emulator or web
         },
--- lib/title_screen.dart
+++ lib/title_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
+
+int Highscore = 5;
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({Key? key}) : super(key: key);
@@ -16,18 +19,18 @@ class TitleScreen extends StatelessWidget {
         appBar: AppBar(title: const Text("Main Menu")),
         body: Center(
           child: Column(
-            children: const [
-              Padding(padding: EdgeInsets.all(50)),
-              Text(
+            children:  [
+              const Padding(padding: EdgeInsets.all(50)),
+              const Text(
                 "Brick Pong",
                 style: TextStyle(fontSize: 50),
               ),
-              Padding(padding: EdgeInsets.all(20)),
-              MenuButton("Play", 60, 150, ),
-              Padding(padding: EdgeInsets.all(5)),
-              MenuButton("How to Play", 60, 150),
-              Padding(padding: EdgeInsets.all(5)),
-              MenuButton("Statistics", 60, 150),
+              const Padding(padding: EdgeInsets.all(20)),
+              MenuButton("Play", 60, 150, () {}),
+              const Padding(padding: EdgeInsets.all(5)),
+              MenuButton("How to Play", 60, 150, () {}),
+              const Padding(padding: EdgeInsets.all(5)),
+              MenuButton("Statistics", 60, 150, () {Navigator.pushNamed(context, "/main_menu/statistics");}),
             ],
           ),
         ),
@@ -42,14 +45,14 @@ class MenuButton extends StatelessWidget {
       this.title,
       this.height,
       this.width,
-      //this.onPressed,
+      this.onPressed,
       {Key? key}
       ) : super(key: key);
 
   final String title;
   final double height;
   final double width;
-  //final Function()? onPressed;
+  final Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +62,7 @@ class MenuButton extends StatelessWidget {
           height: height,
           width: width,
           child: ElevatedButton(
-            onPressed: () {},//onPressed,
+            onPressed: onPressed,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -75,3 +78,67 @@ class MenuButton extends StatelessWidget {
     );
   }
 }
+
+class StatisticsScreen extends StatelessWidget {
+  const StatisticsScreen({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Statistics",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.grey,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Statistics Screen")),
+        body: Center(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.all(50)),
+              const Text(
+                "Statistics",
+                style: TextStyle(fontSize: 50),
+              ),
+              const Text(
+                "High Score",
+                style: TextStyle(fontSize: 20),
+              ),
+              const Padding(padding: EdgeInsets.all(5)),
+              SizedBox(
+                height: 40,
+                width: 100,
+
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(Highscore.toString()),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+
+          backgroundColor: Colors.grey,
+          label: const Text("Back"),
+          icon: const Icon(Icons.arrow_back),
+
+        ),
+      ),
+    );
+  }
+}
+
